a97360fb6f51f772b06592e54b976ae7a1aae057
bvilches <bvilches@gmail.com>
2020-07-27 08:57:09 -0400

    images

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ var config = {
             id: 'chapter1',
             title: 'Latinoamérica, el nuevo epicentro global de COVID-19',
             image: 'images/1-Covid Mundo.png',
-            description: "El virus invisible ha matado casi 600.000 personas en el mundo, con epicentros de casos originalmente en China, Italia, España, Reino Unido y Estados Unidos (con más de 135.000 muertes alrededor del área metropolitana de Nueva York). Sin embargo, con la creciente atención de los medios y autoridades enfocada en la situación del Norte global, el Sur global quedó inadvertido hasta que Brasil se estableció como el segundo país con más casos de coronavirus luego de Estados Unidos. Otros países latinoamericanos siguen los mismos pasos.<span class='credit'> Fuente de datos: <a href='https://coronavirus.jhu.edu/map.html' target='_blank'>John Hopkins CSSE</a></span>",
+            description: "El virus invisible ha matado casi 600.000 personas en el mundo, con epicentros de casos originalmente en China, Italia, España, Reino Unido y Estados Unidos (con más de 135.000 muertes alrededor del área metropolitana de Nueva York). Sin embargo, con la creciente atención de los medios y autoridades enfocada en la situación del Norte global, el Sur global quedó inadvertido hasta que Brasil se estableció como el segundo país con más casos de coronavirus luego de Estados Unidos. Otros países latinoamericanos siguen los mismos pasos.<span class='credit'>Fuente: <a href='https://coronavirus.jhu.edu/map.html' target='_blank'>John Hopkins CSSE</a>.</span>",
             location: {
                 center: [-98, 10],
                 mobileCenter: [-85, 10],
@@ -52,7 +52,8 @@ var config = {
             id: 'picture2',
             type: 'pic',
             title: "La densidad urbana, un falso culpable",
-            description: "La forma en la que el virus se ha propagado en cada lugar del mundo está directamente relacionada con las características demográficas e inequidades presentes en ellos. Inicialmente en Nueva York se había responsabilizado erróneamente a la densidad (habitantes por metro cuadrado) de la ciudad por la gran cantidad de casos y muertes por coronavirus.<span class='credit'>Crédito de Imagen: <a href='https://ny.curbed.com/2020/6/18/21295554/new-york-city-apartments-home-for-sale-plummet' target='_blank'>Curbed</a></span>",
+            image: "images/manhattan-density-curbed.jpg",
+            description: "La forma en la que el virus se ha propagado en cada lugar del mundo está directamente relacionada con las características demográficas e inequidades presentes en ellos. Inicialmente en Nueva York se había responsabilizado erróneamente a la densidad (habitantes por metro cuadrado) de la ciudad por la gran cantidad de casos y muertes por coronavirus.<span class='credit'>Crédito imagen: <a href='https://ny.curbed.com/2020/6/18/21295554/new-york-city-apartments-home-for-sale-plummet' target='_blank'>Curbed</a>.</span>",
             location: {},
             onChapterEnter: [],
             onChapterExit: []
@@ -61,6 +62,7 @@ var config = {
             id: 'picture3',
             type: 'pic',
             title: "La alta conectividad en Áreas Metropolitanas incrementa el contagio",
+            image: "images/covid-subway.jpg",
             description: "La propagación del virus no depende de un único factor. Culpar exclusivamente a la densidad sería un error grave que apuntaría directamente contra los esfuerzos del desarrollo sostenible a futuro en las ciudades, cita <a href='https://www.tandfonline.com/doi/full/10.1080/01944363.2020.1777891' target='_blank'>un estudio reciente</a>. Las Áreas Metropolitanas de ciudades grandes como Nueva York ofrecen tanto ventajas como desventajas cuando se trata de una pandemia. La alta conectividad de los habitantes a través de los medios de transporte locales e internacionales permite una transmisión del virus más rápida. A mayor conectividad, mayor tasa de infectados.<span class='credit'></a>Crédito de Imagen: <a href='https://www.thestar.com/news/gta/2020/06/18/covid-19-and-transit-what-we-think-we-know-may-be-wrong.html' target='_blank'>The Star</a></span>",
             location: {},
             onChapterEnter: [],
@@ -69,6 +71,7 @@ var config = {
         {//5 Mejor Información y Servicios en Áreas Metropolitanas
             id: 'picture4',
             type: 'pic',
+            image: "images/ba-covid-19-precautions-6.jpeg",
             title: 'Las Áreas Metropolitanas están mejor preparadas para contener el virus',
             description: "Por otro lado, las Áreas Metropolitanas están mejor preparadas para contener una situación de esta envergadura creando medidas ágiles y efectivas que puedan frenar el contagio entre sus habitantes. Además los servicios de salud, infraestructura y profesionales concentrados en estas áreas ofrecen mejores servicios a la comunidad en una pandemia.<span class='credit'>Crédito de Imagen: <a href='http://observatorioamba.org/noticias-y-agenda/noticia/coronavirus-en-la-argentina-como-cambiaran-las-ciudades-con-el-distanciamiento-social' target='_blank'>El Observatorio Metropolitano</a></span>",
             location: {},
@@ -181,6 +184,7 @@ var config = {
         {//8 Condiciones Precarias
             id: 'picture6',
             type: 'pic',
+            image: "images/villa-azul-flyover.jpg",
             title: 'Pobreza y precarias condiciones de habitabilidad',
             description: "Las comunidades más pobres han sido las más afectadas alrededor del mundo. En Buenos Aires, los habitantes de asentamientos informales padecen en mayor medida afecciones como <a href='https://blogs.iadb.org/ideas-matter/en/poor-health-poverty-and-the-challenges-of-covid-19-in-latin-america-and-the-caribbean/' target='_blank'>diabetes, obesidad, hipertensión y malnutrición</a>, convirtiéndolos en grupo de riesgo de contraer el coronavirus. Además, la precariedad de las viviendas y barrios que habitan los residentes de estos asentamientos informales ha sido un factor central en el contagio del COVID-19.<span class='credit'>Crédito de Imagen: <a href='https://www.lanacion.com.ar/sociedad/coronavirus-argentina-temor-vecinos-villa-itati-casos-nid2370454' target='_blank'>Diario La Nación</a></span>",
             location: {},
@@ -278,6 +282,7 @@ var config = {
         {//11 Aislacion impuesta
             id: 'picture8',
             type: 'pic',
+            image: "images/villa-azul-cordoned-off-la-nacion2.jpg",
             title: 'Villas miseria, aislación impuesta',
             description: 'En un atropello a las libertades individuales y colectivas, sumado a las deudas sostenidas con las villas miseria, las autoridades locales, regionales y nacionales decidieron acordonar las Villas Itatí y Azul del resto de la ciudad, enfatizando todavía más su evidente segregación urbana y social. Por medio de una aislación obligatoria reforzada por la policía, ambos asentamientos fueron "desconectados" de su entorno por 14 días suscitando una reacción crítica por parte de los mismos habitantes y organizaciones de derechos humanos que reclamaron que dichas medidas empeorarían la innegable situación de aislamiento socio-económico, espacial y de salud ya existentes en las villas miseria. El gobernador de la Provincia de Buenos Aires Kiciloff retrucó diciendo que hubiera tomado la misma medida extremista de aislamiento en caso de tratarse de un pico de casos en un barrio privado. Preocupa la soltura con la que aboga por disminuir las libertades de los habitantes que representa y su falta de coherencia al creer que fuera posible un pico de casos de COVID-19 en un afluente barrio privado donde los recursos, el espacio y la información abundan en calidad y cantidad para evitar los contagios.',
             location: {},
@@ -325,6 +330,7 @@ var config = {
         {//13 Itati y Azul Precariedad
             id: 'picture9',
             type: 'pic',
+            image: "images/villa-azul-precaria_la-nacion.jpg",
             title: 'Villas Itatí y Azul, precariedad extrema',
             description: "En 2018, el 70% de las viviendas en estos asentamientos fueron catalogadas como inestables y precarias, indicando que en 8 años las condiciones calamitosas de habitabilidad no habían cambiado. Casas de chapa, lona, plástico, cartón, entre otros, conforman la paleta constructiva en esta zona. Los materiales reciclables no son el problema, sino la desidia y la falta de voluntad para reintegrar esta isla urbana en un marco de desarrollo sostenible que provea oportunidades de trabajo y movilidad social a los más vulnerables de la sociedad.",
             location: {},
@@ -374,6 +380,7 @@ var config = {
         {//15 Itati y Azul sin Agua ni Servicios
             id: 'picture10',
             type: 'pic',
+            image: "images/villa-azul-no-water-pagina12.jpeg",
             title: 'Villas Itatí y Azul, sin agua',
             description: 'La falta de agua en Itatí y Azul ha sido clave en la propagación del coronavirus junto con el hacinamiento crítico, las enfermedades crónicas y la falta de acceso a los servicios de sanidad. Todos ellos componen cada uno de los ejes del índice de vulnerabilidad que desprotege a las personas del virus y los convierte en víctimas aseguradas de la pandemia.<br /><br />La Organización Mundial de la Salud indica que los sistemas de higiene y saneamiento son fundamentales para proteger a las personas durante el brote del COVID-19. La práctica continua de medidas de higiene, lavado y desinfección de manos y tratamiento de basura correspondientes en las comunidades logran prevenir en gran medida la transmisión del virus entre personas.',
             location: {},
@@ -387,6 +394,7 @@ var config = {
         {//16 Futuro sostenible y equitativo
             id: 'picture7',
             type: 'pic',
+            image: "images/Satellite.png",
             title: '¿Un futuro equitativo, sostenible y con mayor oportunidades?',
             description: 'El manejo improvisado de los gobernantes y la decisión extremista de aislar comunidades por el brote de COVID-19 ha dejado expuesta la corrupción y falta de planificación sostenible en los países latinoamericanos, exacerbando la pobreza e insalubridad de los grupos más vulnerables de la población causando efectos adversos e irreversibles en la economía y acechando la salud pública de las ciudades. La implementación de la <a href="https://www.onu.org.ar/agenda-post-2015/" target="_blank">Agenda de Desarrollo Sostenible</a> es más necesaria que nunca y debe hacerse posible con participación de todas las comunidades. Además de viviendas y servicios dignos, las familias que habitan asentamientos precarios como Villa Itatí y Azul deben poder tener una voz y participación en el proceso de reconstrucción a corto y largo plazo, así proveyendo de oportunidades de trabajo a los residentes y no exclusivamente de espacios para habitar. El espacio público, muchas veces ausente en los barrios populares, se debe considerar como intrínseco y central en la generación de ciudad equitativa y resiliente para todos.<span class="credit">Crédito de Imagen: <a href="https://earth.google.com/web/@-34.70971363,-58.30417164,8.08277571a,5194.29804593d,35y,-0h,0t,0r" target="_blank">Google Earth</a>',
             location: {},
